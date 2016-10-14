d3bedc59fa6ab690f419c6b2f5897461b620c009
Arun-Hackmania <arunmozhimoto@gmail.com>
2016-10-14 22:50:31 +0530

    Json change

--- receiveData.lua
+++ receiveData.lua
@@ -1,11 +1,19 @@
 function GetData()
-    http.get("http://pubsub.pubnub.com/history/sub-c-f224d246-36bb-11e6-ac64-0619f8945a4f/RIngHackMania/0/1", nil, function(code, data)
-    if (code < 0) then
-      print("HTTP request failed")
-    else
-      print("Voltage = " .. data .. " V")
-    end
-  end)
+  http.get("http://pubsub.pubnub.com/history/sub-c-f224d246-36bb-11e6-ac64-0619f8945a4f/RIngHackMania/0/1", nil, function(code, data)
+      if (code < 0) then
+        print("HTTP request failed")
+      else
+        jsonData = nil
+        jsonData = string.match(data,'{.*}')     
+        print(jsonData)
+        if(string.find(jsonData,'id_1..1') ~= nil) then
+                   gpio.write(4,gpio.HIGH)
+        else
+          print("Reset button Two")
+          gpio.write(4,gpio.LOW)
+        end
+      end
+    end)
 end
 
 -- every 5 seconds, check ThingSpeak
