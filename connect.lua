a092221c9ac0aaf6949058ca151203383a48bbbb
Arun-Hackmania <arunmozhimoto@gmail.com>
2016-10-15 08:08:57 +0530

    Moved to Home-Automation-ESP

--- connect.lua
+++
@@ -1,8 +0,0 @@
-wifi.setmode(wifi.STATION)
-wifi.sta.config("Arunmozhi","q1w2e3r4")
-wifi.sta.connect()
-tmr.alarm(1, 1000, 1, function() 
-if wifi.sta.getip()== nil then print("WiFi not connected") 
-else tmr.stop(1) tmr.stop(6) print("Connection done, IP is "..wifi.sta.getip())
-end end)
-
